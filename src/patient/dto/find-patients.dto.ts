e5c7077e8639877fdc359699a8157e8e6afd2701
bl0pez <blopez4434@gmail.com>
2025-01-20 00:00:06 -0300

    feat: enhance patient search functionality with flexible query parameters

--- src/patient/dto/find-patients.dto.ts
+++ src/patient/dto/find-patients.dto.ts
@@ -1,28 +1,31 @@
-import { PatientStatus } from "@prisma/client";
-import { Type } from "class-transformer";
-import { IsEnum, IsNumber, IsOptional, IsPositive, IsString } from "class-validator";
+import { PatientStatus } from '@prisma/client';
+import { Type } from 'class-transformer';
+import {
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
 
 export class findPatientsDto {
-    @Type(() => Number)
-    @IsNumber()
-    @IsPositive()
-    @IsOptional()
-    id?: number;
+  @IsString()
+  @IsOptional()
+  search?: string;
 
-    @IsEnum(PatientStatus)
-    @IsOptional()
-    status?: PatientStatus;
+  @IsEnum(PatientStatus)
+  @IsOptional()
+  status?: PatientStatus;
 
-    @Type(() => Number)
-    @IsNumber()
-    @IsPositive()
-    @IsOptional()
-    page?: number;
+  @Type(() => Number)
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  page?: number;
 
-    @Type(() => Number)
-    @IsNumber()
-    @IsPositive()
-    @IsOptional()
-    limit?: number;
-
+  @Type(() => Number)
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  limit?: number;
 }
--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -7,64 +7,71 @@ import { createPagination } from 'src/helpers/createPagination';
 
 @Injectable()
 export class PatientService {
-    constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-    async findAll(query: findPatientsDto) {
+  async findAll(query: findPatientsDto) {
+    const patientId = isNaN(Number(query?.search))
+      ? undefined
+      : Number(query?.search);
 
-        const patients = await this.prismaService.patient.findMany({
-            take: query?.limit,
-            skip: query?.limit * (query?.page - 1 ) || 0,
-            where: {
-                id: query?.id,
-                status: query?.status
-            }
-        });
+    const patients = await this.prismaService.patient.findMany({
+      take: query?.limit,
+      skip: query?.limit * (query?.page - 1) || 0,
+      where: {
+        AND: [
+          { id: patientId },
+          { name: { contains: query?.search, mode: 'insensitive' } },
+          { rut: { contains: query?.search, mode: 'insensitive' } },
+        ],
+        status: query?.status,
+      },
+    });
 
-        const size = await this.prismaService.patient.count({
-            where: {
-                id: query?.id,
-                status: query?.status
-            }
-        });
+    const size = await this.prismaService.patient.count({
+      //   where: {
+      //     id: query?.id,
+      //     status: query?.status,
+      //   },
+    });
 
-        return {
-            patients: patients,
-            metadata: createPagination({
-                page: query.page || 1,
-                take: query.limit,
-                size: size,
-                count: patients.length,
-            }),
-        }
+    return {
+      patients: patients,
+      metadata: createPagination({
+        page: query.page || 1,
+        take: query.limit,
+        size: size,
+        count: patients.length,
+      }),
+    };
+  }
+
+  async create(createPatientDto: CreatePatientDto) {
+    const patient = await this.findOneByRut(createPatientDto.rut);
+
+    if (patient) {
+      throw new ConflictException('Paciente ya registrado');
     }
 
-    async create(createPatientDto: CreatePatientDto) {
-        const patient = await this.findOneByRut(createPatientDto.rut);
+    return this.prismaService.patient.create({
+      data: {
+        rut: createPatientDto.rut,
+        name: createPatientDto.name,
+        status: PatientStatus.ACTIVE,
+      },
+    });
+  }
 
-        if (patient) {
-            throw new ConflictException('Paciente ya registrado');
-        };
+  async findOneByRut(rut: string) {
+    const patient = await this.prismaService.patient.findFirst({
+      where: {
+        rut: rut,
+      },
+    });
 
-        return this.prismaService.patient.create({
-            data: {
-                rut: createPatientDto.rut,
-                name: createPatientDto.name,
-                status: PatientStatus.ACTIVE
-            }
-        });
+    if (!patient) {
+      return null;
     }
 
-    async findOneByRut(rut: string) {
-        const patient = await this.prismaService.patient.findFirst({
-            where: {
-                rut: rut
-            }
-        });
-
-        if (!patient) {
-            return null;
-        }
-
-        return patient;
-    }
+    return patient;
+  }
 }
