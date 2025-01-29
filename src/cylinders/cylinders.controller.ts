4a5a61b8ac567783c684f168cb01fa6415e5c98b
bl0pez <blopez4434@gmail.com>
2025-01-29 17:38:08 -0300

    feat: add findAll endpoint to CylindersController with pagination support

--- src/cylinders/cylinders.controller.ts
+++ src/cylinders/cylinders.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CylindersService } from './cylinders.service';
 import { CreateCylinderDto } from './dto/create-cylinder.dto';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { Role } from 'src/interfaces';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('cylinders')
 export class CylindersController {
@@ -12,5 +13,11 @@ export class CylindersController {
   @Auth(Role.OPERATOR, Role.ADMIN)
   create(@Body() createCylinderDto: CreateCylinderDto) {
     return this.cylindersService.create(createCylinderDto);
+  }
+
+  @Get()
+  @Auth()
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.cylindersService.findAll(paginationDto);
   }
 }
--- src/cylinders/cylinders.service.ts
+++ src/cylinders/cylinders.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCylinderDto } from './dto/create-cylinder.dto';
 import { CylinderStatus } from '@prisma/client';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { createPagination } from 'src/helpers/createPagination';
 
 @Injectable()
 export class CylindersService {
@@ -14,5 +16,40 @@ export class CylindersService {
         status: CylinderStatus.IN_STOCK,
       },
     });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const isPagination = paginationDto.page && paginationDto.limit;
+
+    const [cylinders, count] = await Promise.all([
+      this.prismaService.cylinder.findMany({
+        skip: isPagination && (paginationDto.page - 1) * paginationDto.limit,
+        take: isPagination && paginationDto.limit,
+        where: {
+          serialNumber: {
+            contains: paginationDto?.search,
+            mode: 'insensitive',
+          },
+        },
+      }),
+      this.prismaService.cylinder.count({
+        where: {
+          serialNumber: {
+            contains: paginationDto?.search,
+            mode: 'insensitive',
+          },
+        },
+      }),
+    ]);
+
+    return {
+      cylinders,
+      metadata: createPagination({
+        page: paginationDto.page,
+        rowsPerPage: paginationDto.limit,
+        count: cylinders.length,
+        totalCount: count,
+      }),
+    };
   }
 }
