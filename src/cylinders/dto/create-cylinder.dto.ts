ee1f8895911e0567c4cfcc00ce6e6a02e2f0de47
bl0pez <blopez4434@gmail.com>
2025-01-30 20:56:44 -0300

    feat: add minimum length validation to serialNumber in CreateCylinderDto

--- src/cylinders/dto/create-cylinder.dto.ts
+++ src/cylinders/dto/create-cylinder.dto.ts
@@ -1,8 +1,9 @@
 import { CylinderCapacity } from '@prisma/client';
-import { IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsString, MinLength } from 'class-validator';
 
 export class CreateCylinderDto {
   @IsString()
+  @MinLength(2)
   serialNumber: string;
 
   @IsEnum([CylinderCapacity.SIX_M3, CylinderCapacity.SIX_M3])
